e5ad2245ed05930971deb03b84cfa342ef9d5e74
mercury <mercurialously@gmail.com>
2020-10-28 20:42:14 +0400

    registration: save user in db

--- Greenatom/Switter/src/main/java/com/example/switter/controller/MainController.java
+++ Greenatom/Switter/src/main/java/com/example/switter/controller/MainController.java
@@ -1,8 +1,10 @@
-package com.example.switter;
+package com.example.switter.controller;
 
 import com.example.switter.domain.Message;
+import com.example.switter.domain.User;
 import com.example.switter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,27 +14,28 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")
-                                   String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
---
+++ Greenatom/Switter/src/main/java/com/example/switter/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.switter.repos;
+
+import com.example.switter.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
