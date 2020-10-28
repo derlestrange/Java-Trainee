6d92117da9287b4d5281b5e06965b6c786b41b83
mercury <mercurialously@gmail.com>
2020-10-28 17:08:57 +0400

    adding message and filter message

---
+++ Greenatom/Switter/src/main/java/com/example/switter/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.switter.repos;
+
+import com.example.switter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long>{
+    List<Message> findByTag(String tag);
+}
