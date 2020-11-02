ed453849e459c714e1619fe2be6c7bb18c70ebd2
mercury <mercurialously@gmail.com>
2020-11-02 23:13:58 +0400

    replace mustache -> ftlh

--- Greenatom/Switter/src/main/java/com/example/switter/domain/Message.java
+++ Greenatom/Switter/src/main/java/com/example/switter/domain/Message.java
@@ -1,25 +1,55 @@
 package com.example.switter.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Message {
+    public Message() {
+    }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public String getAuthorName(){
+        return author!=null ? author.getUsername() : "<none>";
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String text;
     private String tag;
 
-    public Message(){
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Integer getId() {
+        return id;
     }
 
-    public Message(String text, String tag) {
-        this.text = text;
-        this.tag = tag;
+    public void setId(Integer id) {
+        this.id = id;
     }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
 
     public String getText() {
         return text;
